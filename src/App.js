722b09c9b105c667d16b318768868df8e93018be
zhin <zheen99abubakr@gmail.com>
2021-07-14 23:20:40 +0300

    fetch the API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState, useCallback } from "react";
 
 function App() {
+  const [card, setCard] = useState([]);
+  const [value, setValue] = useState([]);
+  const [secondCard, setSecondCard] = useState([]);
+  const [secondValue, setSecondValue] = useState([]);
+  const [message, setMessage] = useState("");
+
+  const fetchMyAPI = useCallback(async () => {
+    let response = await fetch(
+      "https://deckofcardsapi.com/api/deck/new/draw/?count=2"
+    );
+    response = await response.json();
+    setCard(response.cards[0].image);
+    setValue(response.cards[0].value);
+    setSecondCard(response.cards[1].image);
+    setSecondValue(response.cards[1].value);
+  }, []);
+
+  useEffect(() => {
+    fetchMyAPI();
+  }, [fetchMyAPI]);
+
+  const messages = () => {
+    if (value > secondValue) {
+      setMessage("win");
+    } else {
+      setMessage("lose");
+    }
+  };
+  console.log(message);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        </p>
-      </header>
+    <div>
+      <h1>Hello React!</h1>
     </div>
   );
 }
